071bb05b4195fc6c4bef7dc688e0a0ea247eeba9
raizin4 <azagu1096@gmail.com>
2023-01-12 10:42:49 -0500

    agregando funcion inciarJuego y el addEventListener

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,7 +1,19 @@
-function seleccionarMascotaJugador(){
-    alert('SELECCIONASTE TU MASCOTA')
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    if(document.getElementById('hipodoge').checked){
+        alert("SELECCIONASTE A HIPODOGE")
+    } else if (document.getElementById('capipepo').checked){
+        alert("SELECCIONASTE A CAPIPEPO")
+    } else if(document.getElementById('ratigueya').checked){
+        alert("SELECCIONASTE A RATIGUEYA")
+    }
+}
 
+
+
+
+window.addEventListener('load',iniciarJuego)
